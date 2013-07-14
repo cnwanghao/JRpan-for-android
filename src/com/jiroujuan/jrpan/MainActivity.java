b41924f8a0bf34d4dd90770b711dcfc0ff18b062
cnwanghao <wanghao@wanghao-PC>
2013-07-14 17:05:06 +0800

    Signed-off-by: cnwanghao <wanghao@wanghao-PC>

--- src/com/jiroujuan/jrpan/MainActivity.java
+++ src/com/jiroujuan/jrpan/MainActivity.java
@@ -1,7 +1,9 @@
 package com.jiroujuan.jrpan;
 
 import android.os.Bundle;
+import android.os.Handler;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
 import android.view.Window;
 import android.view.WindowManager;
@@ -17,6 +19,14 @@ public class MainActivity extends Activity {
         this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, 
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
 		setContentView(R.layout.activity_main);
+		new Handler().postDelayed(new Runnable(){
+			public void run(){
+				Intent intent = new Intent (MainActivity.this,LoginActivity.class);
+				startActivity(intent);
+				finish();
+			}
+		},1500);
+	        
 	}
 
 	@Override
